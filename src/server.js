18a9a0b1abd54ac49036bcfe8d0fbd37888a05fb
Meru45 <87946072+Meru45@users.noreply.github.com>
2024-03-02 14:47:34 +0530

    Mongo js connection issue solved

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require("http");
 require("dotenv").config();
 
 const app = require("./app");
-const { mongoConnect } = require("./services/mongo");
+const { mongoConnect } = require("../services/mongo");
 
 const PORT = process.env.PORT || 4000;
 
