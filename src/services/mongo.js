e03552b1bd294957beaa8c992c1ac82bb1cf6cc1
Meru45 <87946072+Meru45@users.noreply.github.com>
2024-03-03 11:45:28 +0530

    final commit

--- src/services/mongo.js
+++ src/services/mongo.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const MONGO_URL = `mongodb+srv://CaffinatedCoders:${password}@cluster0.d5c0kvu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const MONGO_URL = `mongodb+srv://CaffinatedCoders:${password}@cluster0.d5c0kvu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// const MONGO_URL = "mongodb://localhost:27017";
+const MONGO_URL = "mongodb://localhost:27017";
 
 mongoose.connection.once("open", () => {
     console.log("MongoDB, connection ready");
